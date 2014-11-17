dd3c35bc4f59f1003f632a034b6ce71b5a3f01b0
andrespineda23 <andres.pin51@hotmail.es>
2014-11-17 10:58:09 -0500

    Creación Repositorio del Proyecto SIRELAB-UC Proyecto de Grado Universidad Central

---
+++ SIRELAB-UC/SIRELAB-UC-ejb/src/java/com/sirelab/dao/PersonaDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sirelab.dao;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author ANDRES PINEDA
+ */
+@Stateless
+@LocalBean
+public class PersonaDAO {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
